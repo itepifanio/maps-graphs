85508468ceddc7c12a718a1e3ca00f1324544238
Juan-Diego Gonzales <juan-diego.gonzales@de.abb.com>
2016-12-08 16:29:53 +0100

    improve precision of road intersections

---
+++ script.py
@@ -0,0 +1,45 @@
+import sys; sys.dont_write_bytecode = True # avoid .pyc files
+
+import json
+import overpy
+
+overpy = overpy.Overpass()
+
+txt = open("nodes.txt", "w")
+
+
+result = overpy.query("""
+<osm-script>
+    <query type="way" into="hw">
+      <has-kv k="highway"/>
+      <has-kv k="highway" modv="not" regv="footway|cycleway|path|service|track"/>
+      <bbox-query e="-77.039006" n="-12.064528" s="-12.093128" w="-77.063135"/>
+    </query>
+
+    <foreach from="hw" into="w">
+      <recurse from="w" type="way-node" into="ns"/>
+      <recurse from="ns" type="node-way" into="w2"/>
+      <query type="way" into="w2">
+        <item set="w2"/>
+        <has-kv k="highway"/>
+        <has-kv k="highway" modv="not" regv="footway|cycleway|path|service|track"/>
+      </query>
+      <difference into="wd">
+        <item set="w2"/>
+        <item set="w"/>
+      </difference>
+      <recurse from="wd" type="way-node" into="n2"/>
+      <recurse from="w"  type="way-node" into="n3"/>
+      <query type="node">
+        <item set="n2"/>
+        <item set="n3"/>
+      </query>
+      <print/>
+    </foreach>
+</osm-script>
+ """)
+
+#print json.dumps(result.nodes, indent=2, default=str)
+print len(result.nodes)
+txt.write(str(result.nodes))
+txt.close()
---
+++ ways.py
@@ -0,0 +1,48 @@
+import sys; sys.dont_write_bytecode = True # avoid .pyc files
+import json
+import osmapi; osmapi = osmapi.OsmApi()
+import folium
+
+# array of objects (list of dict), bounding box for Jesus Maria, Lima, Peru
+bbox = osmapi.Map(-77.063135, -12.093128, -77.039006, -12.064528)
+
+# array of objects (list of dict), containing filtered ways, actual streets and avenues
+ways = []
+nds = []
+
+for result in bbox:
+    tag = result['data']['tag']
+    # check if we are talking about a street (has name and highway within its tags)
+    if result['type'] == 'way' and 'name' in tag.keys() and 'highway' in tag.keys():
+        ways.append(result)
+        '''
+        nodeIds = result['data']['nd']
+        #if result['data']['tag']['highway'] is not ("footway" or "cycleway" or "path" or "service" or "track"):
+        ways.append({
+            'streetId': result['data']['id'],
+            'streetName': tag['name'],
+            'nodesInStreet': nodeIds
+        })
+        '''
+
+for way in ways:
+    id = way['data']['id']
+    nodeIds = way['data']['nd']
+    for nd in nodeIds:
+        waysOfnode = osmapi.NodeWays(nd)
+        for result in waysOfnode:
+            tag = result['tag']
+            waysOfnode.remove(result)
+            if 'name' in tag.keys() and 'highway' in tag.keys():
+                waysOfnode.append(result)
+        for way in waysOfnode:
+            id2 = way['id']
+            if id2 is not id:
+                nds.append(nd)
+
+# remove duplicates
+nds = list(set(nds))
+
+#print json.dumps(ways, indent=2, default=str)
+print nds
+print len(nds)
